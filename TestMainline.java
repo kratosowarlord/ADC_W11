77211c21dc1cf094632aa0bc6808c4afef483795
Karan Kaushal <42567084+kratosowarlord@users.noreply.github.com>
2019-10-06 22:39:28 +0530

    Add files via upload

---
+++ TestMainline.java
@@ -0,0 +1,14 @@
+package nQueens;
+
+public class TestMainline {
+
+	public static void main(String[] args) {
+		System.out.println("Dayanidhee Singh");
+		System.out.println("Solution using Recursion");
+		GameBoard Test5By5 = new GameBoard(5);
+		//Test5By5.place(0, 1);
+		Test5By5.findOneSolution(Test5By5, 0);
+		System.out.println(Test5By5);
+	}
+
+}
